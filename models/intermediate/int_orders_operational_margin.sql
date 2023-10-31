0e3c9deb859c931894ff7ec5aa929368cdfa5687
justinebouretlewagon@gmail.com <justinebouretlewagon@gmail.com>
2023-10-31 15:26:05 +0000

    correct operational margin

--- models/intermediate/int_orders_operational_margin.sql
+++ models/intermediate/int_orders_operational_margin.sql
@@ -1,13 +1,8 @@
-WITH join_t AS (SELECT margin.orders_id
+SELECT margin.orders_id
 ,margin.date_date
-,(margin.margin + ship_t.shipping_fee - ship_t.logcost  - ship_t.ship_cost ) AS operational_margin
+,ROUND(SUM(margin.margin) + SUM(CAST(ship_t.shipping_fee AS FLOAT64)) - SUM(CAST(ship_t.logcost AS FLOAT64)) - SUM(CAST(ship_t.ship_cost AS FLOAT64)),2) AS operational_margin
 FROM {{ ref("int_sales_margin")}} AS margin
 INNER JOIN {{ref("stg_raw__ship")}} AS ship_t
-USING(orders_id))
-SELECT orders_id
-,date_date
-,SUM(operational_margin)
-FROM join_t
-GROUP BY orders_id, date_date
-ORDER BY orders_id DESC, date_date
-
+USING(orders_id)
+GROUP BY margin.orders_id, margin.date_date
+ORDER BY margin.orders_id DESC , margin.date_date
