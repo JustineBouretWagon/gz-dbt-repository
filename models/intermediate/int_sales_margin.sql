d8227f454363b0b2e90a699006ac4999a5ffeb3a
justinebouretlewagon@gmail.com <justinebouretlewagon@gmail.com>
2023-11-01 14:47:42 +0000

    package dbt utils

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,7 +1,7 @@
 SELECT *
 ,(revenue - quantity)  * purchase_price AS margin
 ,quantity* purchase_price AS purchase_cost
-,{{margin_percent("revenue","quantity*purchase_price")}}
+,{{margin_percent("revenue","quantity*purchase_price")}} AS margin_percent_c
 
 FROM {{ ref("stg_raw__sales")}}
 INNER JOIN {{ ref("stg_raw__product")}} 
