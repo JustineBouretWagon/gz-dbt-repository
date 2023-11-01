a707c7ff1a7eaeb9fed4294cb50f9163359bcad5
justinebouretlewagon@gmail.com <justinebouretlewagon@gmail.com>
2023-11-01 09:41:57 +0000

    ope margin solution

--- models/intermediate/int_orders_operational_margin.sql
+++ models/intermediate/int_orders_operational_margin.sql
@@ -1,9 +1,15 @@
-SELECT margin.orders_id
-,margin.date_date
-
-,ROUND(SUM(margin.margin) + SUM(CAST(ship_t.shipping_fee AS FLOAT64)) - SUM(CAST(ship_t.logcost AS FLOAT64)) - SUM(CAST(ship_t.ship_cost AS FLOAT64)),2) AS operational_margin
-FROM {{ ref("int_sales_margin")}} AS margin
-INNER JOIN {{ref("stg_raw__ship")}} AS ship_t
-USING(orders_id)
-GROUP BY margin.orders_id, margin.date_date
-ORDER BY margin.orders_id DESC , margin.date_date
+SELECT
+  o.orders_id
+  ,o.date_date
+  ,ROUND(o.margin + s.shipping_fee - (s.logcost + s.ship_cost),2) AS operational_margin 
+  ,o.quantity
+  ,o.revenue
+  ,o.purchase_cost
+  ,o.margin
+  ,s.shipping_fee
+  ,s.logcost
+  ,s.ship_cost
+FROM {{ref("int_orders_margin")}} o
+LEFT JOIN {{ref("stg_raw__ship")}} s 
+	USING(orders_id)
+ORDER BY orders_id desc
