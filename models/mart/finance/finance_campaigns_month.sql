5e078b83f0703c8a1ffdc494e057c56fcdeea220
justinebouretlewagon@gmail.com <justinebouretlewagon@gmail.com>
2023-11-01 13:08:00 +0000

    finance campaign month

---
+++ models/mart/finance/finance_campaigns_month.sql
@@ -0,0 +1,6 @@
+SELECT DATE(EXTRACT( YEAR FROM date_date),EXTRACT( MONTH FROM date_date),1) AS date_month
+,ROUND(SUM(ads_margin),2) AS ads_margin
+,ROUND(SUM(average_basket),2) AS average_basket
+,ROUND(SUM(operational_margin),2) AS operational_margin
+FROM {{ref("finance_campaigns_day")}}
+GROUP BY date_month
